fb9b22e59d61092412c73525b3fa85823488f3b6
luis.jara <luis.jara@ext.cemex.com>
2024-12-07 20:02:20 -0600

    [ADD] Adding sp to search data by id

---
+++ Procedures/SP_GetTreeDataById.sql
@@ -0,0 +1,27 @@
+CREATE PROCEDURE `GetTreeDataById`(
+    IN id INT
+)
+BEGIN
+    SELECT 
+        TC.tree_ID, 
+        TC.latitude, 
+        TC.longitude, 
+        TI.common_name, 
+        TI.scientific_name,
+        TI.trunk_diameter,
+        TI.canopy_width,
+        TI.height,
+        TI.age,
+        TI.tree_id
+    FROM 
+        TreesCorde AS TC
+    INNER JOIN 
+        TreeInfo AS TI 
+        ON TC.tree_id = TI.tree_id
+    WHERE 
+        TC.tree_id = id;
+END
+
+DROP PROCEDURE IF EXISTS GetTreeDataById;
+
+CALL GetTreeDataById(1);
